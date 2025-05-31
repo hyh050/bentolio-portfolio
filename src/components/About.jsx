ffb779b2a74fe01372efd14a935b0ea356e9d6a2
hyh050 <141797347+hyh050@users.noreply.github.com>
2025-05-31 15:30:20 +0900

    init: About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <section className="about" id="about">
+      <p>
+        Julia Huang is an innovative AI artist, renowned for blending cutting-edge technology with creative expression. Based in LA, she crafts unique digital art experiences accessible globally.
+      </p>
+    </section>
+  );
+}
+export default About;
