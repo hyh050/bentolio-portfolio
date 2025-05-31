cb5b00f2ad17872cf478989c4ad86870f4c0089e
hyh050 <141797347+hyh050@users.noreply.github.com>
2025-05-31 15:30:22 +0900

    init: Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+function Contact() {
+  return (
+    <section className="contact" id="contact">
+      <div className="contact-title">Contact me</div>
+      <div className="contact-question">Have some questions?</div>
+    </section>
+  );
+}
+export default Contact;
