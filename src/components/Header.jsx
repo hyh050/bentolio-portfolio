4838528327cb7a61f6798217a9b228bf500dbbf0
hyh050 <141797347+hyh050@users.noreply.github.com>
2025-05-31 15:30:17 +0900

    init: Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">JULIA HUANG</div>
+      <nav className="nav-links">
+        <a href="#work">PROJECTS</a>
+        <a href="#about">ABOUT</a>
+        <a href="#contact">CONTACT</a>
+      </nav>
+    </header>
+  );
+}
+export default Header;
