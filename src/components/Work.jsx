be914d86bdada61db65bcf57b13575e80512cf60
hyh050 <141797347+hyh050@users.noreply.github.com>
2025-05-31 15:30:21 +0900

    init: Work.jsx

---
+++ src/components/Work.jsx
@@ -0,0 +1,13 @@
+function Work() {
+  return (
+    <section className="work" id="work">
+      <div className="work-list">
+        <div className="work-item">Musea</div>
+        <div className="work-item">Elara</div>
+        <div className="work-item">Verve</div>
+        <div className="work-item">Zephyr</div>
+      </div>
+    </section>
+  );
+}
+export default Work;
